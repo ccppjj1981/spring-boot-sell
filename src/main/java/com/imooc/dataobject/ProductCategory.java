2e642186ee0ee796e0ebcf815ff22aed9f1813ae
ccppjj1981 <ccppjj1981115@163.com>
2018-06-02 00:09:21 +0800

    init

---
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -0,0 +1,11 @@
+package com.imooc.dataobject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: macbook
+ * Date: 18/6/2
+ * Time: 上午12:08
+ * Description: No Description
+ */
+public class ProductCategory {
+}
